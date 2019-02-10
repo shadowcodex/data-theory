7d0a07882d808f98680dda3e051f59d8e49a8694
shadowcodex <jsduncan98@gmail.com>
2019-02-10 00:13:00 -0600

    basic tests

--- test/sample.js
+++ test/sample.js
@@ -1,13 +1,77 @@
 var assert = require('assert');
 let ptime = require('quick-ptime');
+const { stacks } = require('../index');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', async function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-      await new Promise((resolve, reject) => {
-        setTimeout(resolve, 1000);
-      })
-    });
+describe('Stack', function() {
+  it('should make a new stack', function() {
+    const stack = new stacks.Stack();
+    assert.equal(stack.height(), 0);    
+  });
+
+  it('should push an item on to the stack', function() {
+    let stack = new stacks.Stack();
+    stack.push('item');
+
+    assert.equal(stack.peek(), 'item');
+  });
+
+  it('should pop an item off the stack', function() {
+    let stack = new stacks.Stack();
+    stack.push('item');
+    let value = stack.pop();
+    assert.equal(value, 'item');
+  });
+
+  it('should push three items on and pop them off in order', function() {
+    let stack = new stacks.Stack();
+    stack.push('item1');
+    stack.push('item2');
+    stack.push('item3');
+
+    let value = stack.pop();
+    assert.equal(value, 'item3');
+    
+    value = stack.pop();
+    assert.equal(value, 'item2');
+    
+    value = stack.pop();
+    assert.equal(value, 'item1');
+  });
+});
+
+describe('Array Stack', function() {
+  it('should make a new stack', function() {
+    const stack = new stacks.ArrayStack();
+    assert.equal(stack.height(), 0);    
+  });
+
+  it('should push an item on to the stack', function() {
+    let stack = new stacks.ArrayStack();
+    stack.push('item');
+
+    assert.equal(stack.peek(), 'item');
+  });
+
+  it('should pop an item off the stack', function() {
+    let stack = new stacks.ArrayStack();
+    stack.push('item');
+    let value = stack.pop();
+    assert.equal(value, 'item');
+  });
+
+  it('should push three items on and pop them off in order', function() {
+    let stack = new stacks.ArrayStack();
+    stack.push('item1');
+    stack.push('item2');
+    stack.push('item3');
+
+    let value = stack.pop();
+    assert.equal(value, 'item3');
+    
+    value = stack.pop();
+    assert.equal(value, 'item2');
+    
+    value = stack.pop();
+    assert.equal(value, 'item1');
   });
 });
