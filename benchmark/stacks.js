3f167c5a29a92d773ce6f63f03aaf1c18a12254e
shadowcodex <jsduncan98@gmail.com>
2019-03-14 19:34:52 -0500

    fixed stacks benchmark to be size independant

--- benchmark/stacks.js
+++ benchmark/stacks.js
@@ -5,48 +5,91 @@ var ProgressBar = require('progress');
 
 let bar;
 
-const standard = async (name, func, value) => {
-  // console.log("Running...", name);
-  const result = [
-    (await ptime.runFunctionRounds(`${name} 100000`, func, [value], 100000, true)).formatted,
-    (await ptime.runFunctionRounds(`${name} 1000000`, func, [value], 1000000, true)).formatted,
-    (await ptime.runFunctionRounds(`${name} 10000000`, func, [value], 10000000, true)).formatted,
-    (await ptime.runFunctionAverage(`${name} 1`, func, [value], 1000000, true)).formatted,
-  ];
-
-  let str = {};
-  str[name] = result;
-  bar.tick();
-  return str;
-};
-
-const run = async () => {
-  bar = new ProgressBar(':bar :percent :elapseds total, completes in :etas', { total: 11 });
-  let results = [];
-  let linkedStack = new stacks.LinkedStack();
-  let uArrayStack = new stacks.UArrayStack();
-  bar.tick();
-  let bArrayStack = new stacks.BArrayStack(12100000);
-
-  bar.tick();
+const testStandard = async (name, adt, count) => {
+  const iADT = new adt(count);
+  bar.tick({
+    token1: `${name} adt setup`,
+  });
+  results = [];
   results.push(
-    await standard('LinkedStack Push', linkedStack.push.bind(linkedStack), 1),
-    await standard('UArrayStack Push', uArrayStack.push.bind(uArrayStack), 1),
-    await standard('BArrayStack Push', bArrayStack.push.bind(bArrayStack), 1),
-    await standard('LinkedStack Top', linkedStack.top.bind(linkedStack), 1),
-    await standard('UArrayStack Top', uArrayStack.top.bind(uArrayStack), 1),
-    await standard('BArrayStack Top', bArrayStack.top.bind(bArrayStack), 1),
-    await standard('LinkedStack Pop', linkedStack.pop.bind(linkedStack), 1),
-    await standard('UArrayStack Pop', uArrayStack.pop.bind(uArrayStack), 1),
-    await standard('BArrayStack Pop', bArrayStack.pop.bind(bArrayStack), 1)
+    (await ptime.runFunctionRounds(`${name} push ${count}`, iADT.push.bind(iADT), [1], count, true)).formatted
   );
+  bar.tick({
+    token1: `${name} push ${count}`,
+  });
+  results.push(
+    (await ptime.runFunctionRounds(`${name} top ${count}`, iADT.top.bind(iADT), [1], count, true)).formatted
+  );
+  bar.tick({
+    token1: `${name} top ${count}`,
+  });
+  results.push(
+    (await ptime.runFunctionRounds(`${name} pop ${count}`, iADT.pop.bind(iADT), [1], count, true)).formatted
+  );
+  bar.tick({
+    token1: `${name} pop ${count}`,
+  });
 
   return results;
 };
 
+const run = async () => {
+  bar = new ProgressBar(':bar Last Completed :token1 :percent :elapseds total, completes in :etas', { total: 34 });
+  bar.tick({
+    token1: 'progress bar',
+  });
+
+  let results = {};
+
+  results['LinkedStack Push'] = [];
+  results['UArrayStack Push'] = [];
+  results['BArrayStack Push'] = [];
+
+  results['LinkedStack Top'] = [];
+  results['UArrayStack Top'] = [];
+  results['BArrayStack Top'] = [];
+
+  results['LinkedStack Pop'] = [];
+  results['UArrayStack Pop'] = [];
+  results['BArrayStack Pop'] = [];
+
+  bar.tick({
+    token1: 'result sets setup',
+  });
+
+  const l100000 = await testStandard('LinkedStack', stacks.LinkedStack, 100000);
+  const l1000000 = await testStandard('LinkedStack', stacks.LinkedStack, 1000000);
+  results['LinkedStack Push'].push(l100000[0], l1000000[0]);
+  results['LinkedStack Top'].push(l100000[1], l1000000[1]);
+  results['LinkedStack Pop'].push(l100000[2], l1000000[2]);
+
+  const u100000 = await testStandard('UArrayStack', stacks.UArrayStack, 100000);
+  const u1000000 = await testStandard('UArrayStack', stacks.UArrayStack, 1000000);
+  results['UArrayStack Push'].push(u100000[0], u1000000[0]);
+  results['UArrayStack Top'].push(u100000[1], u1000000[1]);
+  results['UArrayStack Pop'].push(u100000[2], u100000[2]);
+
+  const b100000 = await testStandard('BArrayStack', stacks.BArrayStack, 100000);
+  const b1000000 = await testStandard('BArrayStack', stacks.BArrayStack, 1000000);
+  results['BArrayStack Push'].push(b100000[0], b1000000[0]);
+  results['BArrayStack Top'].push(b100000[1], b1000000[1]);
+  results['BArrayStack Pop'].push(b100000[2], b100000[2]);
+
+  let items = [];
+
+  let keys = Object.keys(results);
+  for (let i = 0; i < keys.length; i++) {
+    let item = {};
+    item[keys[i]] = results[keys[i]];
+    items.push(item);
+  }
+
+  return items;
+};
+
 run().then(value => {
   const table = new Table({
-    head: ['Stack Operation', '100,000 items', '1,000,000 items', '10,000,000 items', '1 item AVG'],
+    head: ['Stack Operation', '100,000 items', '1,000,000 items'],
   });
 
   table.push(...value);
