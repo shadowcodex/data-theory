924b343c773274acdbbeac16da68e65c12dd2748
shadowcodex <jsduncan98@gmail.com>
2019-03-15 00:00:11 -0500

    finished repeating code

--- benchmark/queues.js
+++ benchmark/queues.js
@@ -1,103 +1,13 @@
 let { queues } = require('./../index');
-let ptime = require('quick-ptime');
 const Table = require('cli-table');
-var ProgressBar = require('progress');
-
-let bar;
-
-const testStandard = async (name, adt, count) => {
-  const iADT = new adt(count);
-  bar.tick({
-    token1: `${name} adt setup`,
-  });
-  results = [];
-  results.push(
-    (await ptime.runFunctionRounds(`${name} enqueue ${count}`, iADT.enqueue.bind(iADT), [1], count, true)).formatted
-  );
-  bar.tick({
-    token1: `${name} enqueue ${count}`,
-  });
-  results.push(
-    (await ptime.runFunctionRounds(`${name} peek ${count}`, iADT.peek.bind(iADT), [1], count, true)).formatted
-  );
-  bar.tick({
-    token1: `${name} peek ${count}`,
-  });
-  results.push(
-    (await ptime.runFunctionRounds(`${name} dequeue ${count}`, iADT.dequeue.bind(iADT), [1], count, true)).formatted
-  );
-  bar.tick({
-    token1: `${name} dequeue ${count}`,
-  });
-
-  return results;
-};
+const { standardRun } = require('./util');
 
 const run = async () => {
-  bar = new ProgressBar(':bar Last Completed :token1 :percent :elapseds total, completes in :etas', { total: 34 });
-  bar.tick({
-    token1: 'progress bar',
-  });
+  const dataTypes = ['LinkedQueue', 'UArrayQueue', 'BFltArrayQueue', 'BFixArrayQueue'];
+  //   const dataTypes = ['LinkedQueue', 'BFltArrayQueue'];
+  const methods = ['enqueue', 'peek', 'dequeue'];
 
-  let results = {};
-
-  results['LinkedQueue Enqueue'] = [];
-  results['UArrayQueue Enqueue'] = [];
-  results['BFltArrayQueue Enqueue'] = [];
-  results['BFixArrayQueue Enqueue'] = [];
-
-  results['LinkedQueue Peek'] = [];
-  results['UArrayQueue Peek'] = [];
-  results['BFltArrayQueue Peek'] = [];
-  results['BFixArrayQueue Peek'] = [];
-
-  results['LinkedQueue Dequeue'] = [];
-  results['UArrayQueue Dequeue'] = [];
-  results['BFltArrayQueue Dequeue'] = [];
-  results['BFixArrayQueue Dequeue'] = [];
-
-  bar.tick({
-    token1: 'result sets setup',
-  });
-
-  const lQueue100000 = await testStandard('LinkedQueue', queues.LinkedQueue, 100000);
-  const lQueue1000000 = await testStandard('LinkedQueue', queues.LinkedQueue, 1000000);
-  //   const lQueue10000000 = await testStandard('LinkedQueue', queues.LinkedQueue, 10000000);
-  results['LinkedQueue Enqueue'].push(lQueue100000[0], lQueue1000000[0]);
-  results['LinkedQueue Peek'].push(lQueue100000[1], lQueue1000000[1]);
-  results['LinkedQueue Dequeue'].push(lQueue100000[2], lQueue1000000[2]);
-
-  const bfltQueue100000 = await testStandard('BFltArrayQueue', queues.BFltArrayQueue, 100000);
-  const bfltQueue1000000 = await testStandard('BFltArrayQueue', queues.BFltArrayQueue, 1000000);
-  //   const bfltQueue10000000 = await testStandard('BFltArrayQueue', queues.BFltArrayQueue, 10000000);
-  results['BFltArrayQueue Enqueue'].push(bfltQueue100000[0], bfltQueue1000000[0]);
-  results['BFltArrayQueue Peek'].push(bfltQueue100000[1], bfltQueue1000000[1]);
-  results['BFltArrayQueue Dequeue'].push(bfltQueue100000[2], bfltQueue1000000[2]);
-
-  const uQueue100000 = await testStandard('UArrayQueue', queues.UArrayQueue, 100000);
-  const uQueue1000000 = await testStandard('UArrayQueue', queues.UArrayQueue, 1000000);
-  //   const uQueue10000000 = await testStandard('UArrayQueue', queues.UArrayQueue, 10000000);
-  results['UArrayQueue Enqueue'].push(uQueue100000[0], uQueue1000000[0]);
-  results['UArrayQueue Peek'].push(uQueue100000[1], uQueue1000000[1]);
-  results['UArrayQueue Dequeue'].push(uQueue100000[2], uQueue1000000[2]);
-
-  const bfixQueue100000 = await testStandard('BFixArrayQueue', queues.BFixArrayQueue, 100000);
-  const bfixQueue1000000 = await testStandard('BFixArrayQueue', queues.BFixArrayQueue, 1000000);
-  //   const bfixQueue10000000 = await testStandard('BFixArrayQueue', queues.BFixArrayQueue, 10000000);
-  results['BFixArrayQueue Enqueue'].push(bfixQueue100000[0], bfixQueue1000000[0]);
-  results['BFixArrayQueue Peek'].push(bfixQueue100000[1], bfixQueue1000000[1]);
-  results['BFixArrayQueue Dequeue'].push(bfixQueue100000[2], bfixQueue1000000[2]);
-
-  let items = [];
-
-  let keys = Object.keys(results);
-  for (let i = 0; i < keys.length; i++) {
-    let item = {};
-    item[keys[i]] = results[keys[i]];
-    items.push(item);
-  }
-
-  return items;
+  return await standardRun(dataTypes, methods, queues);
 };
 
 run().then(value => {
--- benchmark/stacks.js
+++ benchmark/stacks.js
@@ -1,90 +1,12 @@
 let { stacks } = require('./../index');
-let ptime = require('quick-ptime');
 const Table = require('cli-table');
-var ProgressBar = require('progress');
-
-let bar;
-
-const testStandard = async (name, adt, count) => {
-  const iADT = new adt(count);
-  bar.tick({
-    token1: `${name} adt setup`,
-  });
-  results = [];
-  results.push(
-    (await ptime.runFunctionRounds(`${name} push ${count}`, iADT.push.bind(iADT), [1], count, true)).formatted
-  );
-  bar.tick({
-    token1: `${name} push ${count}`,
-  });
-  results.push(
-    (await ptime.runFunctionRounds(`${name} top ${count}`, iADT.top.bind(iADT), [1], count, true)).formatted
-  );
-  bar.tick({
-    token1: `${name} top ${count}`,
-  });
-  results.push(
-    (await ptime.runFunctionRounds(`${name} pop ${count}`, iADT.pop.bind(iADT), [1], count, true)).formatted
-  );
-  bar.tick({
-    token1: `${name} pop ${count}`,
-  });
-
-  return results;
-};
+const { standardRun } = require('./util');
 
 const run = async () => {
-  bar = new ProgressBar(':bar Last Completed :token1 :percent :elapseds total, completes in :etas', { total: 34 });
-  bar.tick({
-    token1: 'progress bar',
-  });
+  const dataTypes = ['LinkedStack', 'UArrayStack', 'BArrayStack'];
+  const methods = ['push', 'top', 'pop'];
 
-  let results = {};
-
-  results['LinkedStack Push'] = [];
-  results['UArrayStack Push'] = [];
-  results['BArrayStack Push'] = [];
-
-  results['LinkedStack Top'] = [];
-  results['UArrayStack Top'] = [];
-  results['BArrayStack Top'] = [];
-
-  results['LinkedStack Pop'] = [];
-  results['UArrayStack Pop'] = [];
-  results['BArrayStack Pop'] = [];
-
-  bar.tick({
-    token1: 'result sets setup',
-  });
-
-  const l100000 = await testStandard('LinkedStack', stacks.LinkedStack, 100000);
-  const l1000000 = await testStandard('LinkedStack', stacks.LinkedStack, 1000000);
-  results['LinkedStack Push'].push(l100000[0], l1000000[0]);
-  results['LinkedStack Top'].push(l100000[1], l1000000[1]);
-  results['LinkedStack Pop'].push(l100000[2], l1000000[2]);
-
-  const u100000 = await testStandard('UArrayStack', stacks.UArrayStack, 100000);
-  const u1000000 = await testStandard('UArrayStack', stacks.UArrayStack, 1000000);
-  results['UArrayStack Push'].push(u100000[0], u1000000[0]);
-  results['UArrayStack Top'].push(u100000[1], u1000000[1]);
-  results['UArrayStack Pop'].push(u100000[2], u100000[2]);
-
-  const b100000 = await testStandard('BArrayStack', stacks.BArrayStack, 100000);
-  const b1000000 = await testStandard('BArrayStack', stacks.BArrayStack, 1000000);
-  results['BArrayStack Push'].push(b100000[0], b1000000[0]);
-  results['BArrayStack Top'].push(b100000[1], b1000000[1]);
-  results['BArrayStack Pop'].push(b100000[2], b100000[2]);
-
-  let items = [];
-
-  let keys = Object.keys(results);
-  for (let i = 0; i < keys.length; i++) {
-    let item = {};
-    item[keys[i]] = results[keys[i]];
-    items.push(item);
-  }
-
-  return items;
+  return await standardRun(dataTypes, methods, stacks);
 };
 
 run().then(value => {
